31179292bc55a30bb65a57e097bafd20c3d39aec
심철환 <a@akaiv.com>
2021-10-20 20:23:37 +0900

    Add a button to sign only on SendButton

--- src/components/SendButton.tsx
+++ src/components/SendButton.tsx
@@ -1,24 +1,39 @@
 import { useWallet } from "@terra-money/wallet-provider"
-import { MsgSend } from "@terra-money/terra.js"
+import { MsgSend, Tx } from "@terra-money/terra.js"
 import useConnectedAddress from "../hooks/useConnectedAddress"
+import useLCDClient from "../hooks/useLCDClient"
 import { useGasPrice } from "../hooks/useGasPrices"
 
 const SendButton = () => {
-  const { post } = useWallet()
+  const wallet = useWallet()
   const address = useConnectedAddress()
   const gasPrices = useGasPrice("uluna")
+  const lcd = useLCDClient()
 
   if (!gasPrices) return null
 
-  const send = async () => {
-    const msgs = [new MsgSend(address, address, `1uluna`)]
-    const memo = "Test"
-    const txOptions = { msgs, memo, gasPrices }
-    const response = await post(txOptions)
+  const msgs = [new MsgSend(address, address, `1uluna`)]
+  const memo = "Test"
+  const txOptions = { msgs, memo, gasPrices }
+
+  const sign = async () => {
+    const response = await wallet.sign(txOptions)
+    console.log(response.result)
+    const result = await lcd.tx.broadcast(response.result)
+    console.log(result)
+  }
+
+  const post = async () => {
+    const response = await wallet.post(txOptions)
     console.log(response)
   }
 
-  return <button onClick={send}>Send</button>
+  return (
+    <>
+      <button onClick={sign}>Send (Sign)</button>
+      <button onClick={post}>Send</button>
+    </>
+  )
 }
 
 export default SendButton
