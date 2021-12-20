a109bffec1c0f30e85bf55d6f6f427bdea2cf12d
심철환 <a@akaiv.com>
2021-12-20 20:33:09 +0900

    Initialize app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,5 @@
-import { useAddress } from "../hooks/useConnectedAddress"
-import SendButton from "./SendButton"
-import ConnectButton from "./ConnectButton"
-import ExecuteButton from "./ExecuteButton"
-
 const App = () => {
-  const address = useAddress()
-
-  return (
-    <main>
-      <ConnectButton />
-
-      {address && (
-        <section>
-          <SendButton />
-          <ExecuteButton />
-        </section>
-      )}
-    </main>
-  )
+  return null
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,16 @@
 import { StrictMode } from "react"
 import { render } from "react-dom"
 import { QueryClient, QueryClientProvider } from "react-query"
-import { getChainOptions, WalletProvider } from "@terra-money/wallet-provider"
-import { BRIDGE } from "./contants"
 import "./index.scss"
 import App from "./components/App"
 
 const queryClient = new QueryClient()
 
-getChainOptions().then((chainOptions) => {
-  render(
-    <StrictMode>
-      <WalletProvider {...chainOptions} connectorOpts={{ bridge: BRIDGE }}>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </WalletProvider>
-    </StrictMode>,
-    document.getElementById("root")
-  )
-})
+render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
+  document.getElementById("root")
+)
